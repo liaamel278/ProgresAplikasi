243680fdc899412ebd0068026564e737f0e0f5cb
aamamalia <liaamll278@gmail.com>
2023-10-12 08:05:36 +0700

    Initial commit

--- app/src/main/java/com/example/progresapk/MainActivity.kt
+++ app/src/main/java/com/example/progresapk/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.progresapk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.progresapk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.navView.setupWithNavController(navController)
     }
 }
